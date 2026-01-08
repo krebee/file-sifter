eb68aab099d24b6d4dbb01e485f66313d5f3ee8f
krebee <0201yoshi0201@gmail.com>
2026-01-08 22:20:15 +0900

    Resolve XAML merge markers in Presentation main window

--- src/FileSifter/Presentation/Main/MainWindow.xaml.cs
+++ src/FileSifter/Presentation/Main/MainWindow.xaml.cs
@@ -1,11 +1,10 @@
 using FileSifter.Presentation.Main;
 using System.Windows;
-<<<<<<<< HEAD:src/FileSifter/Presentation/Main/MainWindow.xaml.cs
 namespace FileSifter.Presentation;
-========
+
 
 namespace FileSifter.MainWindow;
->>>>>>>> レイヤード構成への全面リファクタリング:src/FileSifter/MainWindow/MainWindow.xaml.cs
+
 
 public partial class MainWindow : Window
 {
